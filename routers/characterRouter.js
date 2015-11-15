4f29dbbaaa18296cd6421ea1d5ba651b5ab966d2
Michael <mjlemay@gmail.com>
2015-11-14 18:33:30 -0800

    adds connection routes for characters

--- routers/characterRouter.js
+++ routers/characterRouter.js
@@ -1,7 +1,9 @@
 //characterRouter.js
 var express = require('express');
 var router = express.Router();
+var Story = require('../models/story');
 var Character = require('../models/character');
+var Tribe = require('../models/tribe');
 var _ = require('lodash');
 
 router.route('/:character_slug')
@@ -64,6 +66,113 @@ router.route('/:character_slug')
       } else {
         res.json({ error: 'Failed to Login.'});
       }
+  });
+
+router.route('/connect/:character_slug/tribe/:tribe_slug')
+  .put(function(req, res) {
+      Character.findOne({slug: req.params.character_slug}, function(err, character) {
+          if (err) {
+            res.json(err);
+          }
+          console.log('character found to edit');
+          if (req.user &&
+            (req.user.is_admin === true ||
+              req.user._id === character.creator_id)) {
+             Tribe.findOne({slug: req.params.tribe_slug}, function(err, tribe) {
+                if (err) {
+                  res.json(err);
+                }  
+                  if (_.indexOf(character.connectedTribes, req.params.tribe_slug) === -1){
+                    character.connectedTribes.push(req.params.tribe_slug);
+                  }
+                  character.save(function(err) {
+                    if (err) {
+                      res.send(err);
+                    }
+                    res.json({ message: 'Story connection updated.' });
+                  });
+            });
+          } else {
+            res.json({
+              message: 'Cannot modify story.'
+            });
+          }
+      });
+  })
+  .delete(function(req, res) {
+      Character.findOne({slug: req.params.character_slug}, function(err, character) {
+          if (err) {
+            res.json(err);
+          }
+          if (req.user &&
+            (req.user.is_admin === true ||
+              req.user._id === character.creator_id)) {
+              character.connectedTribes = _.without(character.connectedTribes, req.params.tribe_slug);
+              character.save(function(err) {
+                if (err) {
+                  res.send(err);
+                }
+                res.json({ message: 'Story connection removed.' });
+              });
+          } else {
+            res.json({
+              message: 'Cannot modify story.'
+            });
+          }
+      });
+  });
+
+router.route('/connect/:character_slug/story/:story_slug')
+  .put(function(req, res) {
+      Character.findOne({slug: req.params.character_slug}, function(err, character) {
+          if (err) {
+            res.json(err);
+          }
+          if (req.user &&
+            (req.user.is_admin === true ||
+              req.user._id === character.creator_id)) {
+             Story.findOne({slug: req.params.story_slug}, function(err, story) {
+                if (err) {
+                  res.json(err);
+                }  
+                  if (_.indexOf(character.connectedStories, req.params.story_slug) === -1){
+                    character.connectedStories.push(req.params.story_slug);
+                  }
+                  character.save(function(err) {
+                    if (err) {
+                      res.send(err);
+                    }
+                    res.json({ message: 'Story connection updated.' });
+                  });
+            });
+          } else {
+            res.json({
+              message: 'Cannot modify story.'
+            });
+          }
+      });
+  })
+  .delete(function(req, res) {
+      Character.findOne({slug: req.params.character_slug}, function(err, character) {
+          if (err) {
+            res.json(err);
+          }
+          if (req.user &&
+            (req.user.is_admin === true ||
+              req.user._id === character.creator_id)) {
+              character.connectedStories = _.without(character.connectedStories, req.params.story_slug);
+              character.save(function(err) {
+                if (err) {
+                  res.send(err);
+                }
+                res.json({ message: 'Story connection removed.' });
+              });
+          } else {
+            res.json({
+              message: 'Cannot modify story.'
+            });
+          }
+      });
   });
 
 module.exports = router;
