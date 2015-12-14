82dcbb195ff17c42e79d11bf389b4fc4204a720f
Michael <mjlemay@gmail.com>
2015-12-13 19:45:22 -0800

    removes console logs. good job and god speed console

--- routers/passport.js
+++ routers/passport.js
@@ -37,6 +37,7 @@ module.exports = function(passport) {
         passReqToCallback : true // allows us to pass back the entire request to the callback
     },
     function(req, email, password, done) {
+
         // asynchronous
         // User.findOne wont fire unless data is sent back
         process.nextTick(function() {
@@ -85,30 +86,23 @@ module.exports = function(passport) {
         passReqToCallback : true // allows us to pass back the entire request to the callback
     },
     function(req, email, password, done) { // callback with email and password from our form
-        console.log(req);
-        console.log(email + ', ' +password);
+
         // find a user whose email is the same as the forms email
         // we are checking to see if the user trying to login already exists
         User.findOne({ 'logins.local.email' :  email }, function(err, user) {
             // if there are any errors, return the error before anything else
-            console.log(user);
-
-            if (err) {
-                console.log(err);
+            if (err)
                 return done(err);
-            }
+
             // if no user is found, return the message
-            if (!user) {
-                console.log('NO USER!');
+            if (!user)
                 return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
-            }
+
             // if the user is found but the password is wrong
-            if (!user.validPassword(password)) {
-                console.log('NO PASSWORD');
+            if (!user.validPassword(password))
                 return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
-            }
+
             // all is well, return successful user
-            console.log('ITS ALL GOOD YO!');
             return done(null, user);
         });
 
