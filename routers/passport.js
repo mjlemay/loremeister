fa13d888258baae8a5c43ace445ac77938456b72
Michael <mjlemay@gmail.com>
2015-12-13 18:54:40 -0800

    debugging live login system

--- routers/passport.js
+++ routers/passport.js
@@ -37,7 +37,7 @@ module.exports = function(passport) {
         passReqToCallback : true // allows us to pass back the entire request to the callback
     },
     function(req, email, password, done) {
-
+        console.log(req, email, password);
         // asynchronous
         // User.findOne wont fire unless data is sent back
         process.nextTick(function() {
