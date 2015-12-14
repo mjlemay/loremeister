ffa29a6cdb9c1dccfd6d5ce34b05b71403823fcf
Michael <mjlemay@gmail.com>
2015-12-13 20:11:26 -0800

    another console log

--- routers/passport.js
+++ routers/passport.js
@@ -86,10 +86,11 @@ module.exports = function(passport) {
         passReqToCallback : true // allows us to pass back the entire request to the callback
     },
     function(req, email, password, done) { // callback with email and password from our form
-
+        console.log(req);
         // find a user whose email is the same as the forms email
         // we are checking to see if the user trying to login already exists
         User.findOne({ 'logins.local.email' :  email }, function(err, user) {
+
             // if there are any errors, return the error before anything else
             if (err)
                 return done(err);
